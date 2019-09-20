8e1ed7db4be77279e8a67b4f2cd6e1e4bc945837
Codehaks <ghods.hakim@gmail.com>
2019-09-20 09:53:41 +0430

    Docs

---
+++ src/Portal/Domain/Entities/Doc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Domain.Entities
+{
+    public class Doc
+    {
+        public int Id { get; set; }
+        public Byte[] Data { get; set; }
+    }
+}
---
+++ src/Portal/Infrastrcuture/ExcelService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Infrastrcuture
+{
+    public class ExcelService
+    {
+
+    }
+}
