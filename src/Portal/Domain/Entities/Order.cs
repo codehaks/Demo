5ee5e27aeef63e8fd264f690477d2eea2d82c299
Codehaks <ghods.hakim@gmail.com>
2019-09-20 09:49:18 +0430

    Entitites

---
+++ src/Portal/Domain/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
