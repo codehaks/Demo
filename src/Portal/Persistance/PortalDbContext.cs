48cfae8dab2e3782152a1e3cfbd11d08ea2f052f
Pized <vegazed7@gmail.com>
2019-09-20 08:28:36 +0430

    Startup

---
+++ src/Portal/Persistance/PortalDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Portal.Identity;
+
+namespace Portal.Persistance
+{
+    public class PortalDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public PortalDbContext(DbContextOptions<PortalDbContext> options)
+                : base(options)
+        {
+        }
+
+       
+
+        public override int SaveChanges()
+        {
+
+            return base.SaveChanges();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder.ApplyConfiguration(new FoodConfig());
+            base.OnModelCreating(builder);
+        }
+    }
+}
