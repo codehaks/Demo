7cc2bf319170542ec51b64c48e799b1f76dc45dc
Codehaks <ghods.hakim@gmail.com>
2019-09-20 08:55:55 +0430

    fix

--- src/Portal.Web/Startup.cs
+++ src/Portal.Web/Startup.cs
@@ -5,17 +5,43 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Portal.Identity;
+using Portal.Persistance;
 
 namespace Portal.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PortalDbContext>(options =>
+            {
+                //options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
+                options.UseSqlite("Data Source=portal.sqlite");
+                options.EnableDetailedErrors();
+                options.EnableSensitiveDataLogging();
+            });
+
+            services.AddDefaultIdentity<ApplicationUser>(
+                options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<PortalDbContext>();
+
+            services.AddRazorPages();
+            services.AddControllers();
+            //services.AddSignalR();
+            //services.AddAutoMapper(typeof(Application.Playing.HandMapper).GetTypeInfo().Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +52,17 @@ namespace Portal.Web
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapRazorPages();
+                
+                endpoints.MapControllers();
             });
         }
     }
