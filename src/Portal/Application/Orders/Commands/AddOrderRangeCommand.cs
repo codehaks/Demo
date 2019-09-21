67b0a0b3e275bbc69f1aaedba449160cb99ab98b
Codehaks <ghods.hakim@gmail.com>
2019-09-21 13:11:42 +0430

    Fix

---
+++ src/Portal/Application/Orders/Commands/AddOrderRangeCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portal.Application.Orders.Commands
+{
+    public class OrderRawInfo
+    {
+
+        public string CustomerName { get; set; }
+        public string CityName { get; set; }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+
+    }
+    public class AddOrderRangeCommand : IRequest<bool>
+    {
+        public IEnumerable<OrderRawInfo> Orders { get; set; }
+    }
+}
---
+++ src/Portal/Application/Orders/Commands/AddOrderRangeCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Portal.Application.Orders.Commands
+{
+    class AddOrderRangeCommandHandler : IRequestHandler<AddOrderRangeCommand, bool>
+    {
+        public Task<bool> Handle(AddOrderRangeCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
